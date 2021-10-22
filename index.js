f36c7156ec67020885f653353490a210dd7885e9
MarioG8 <golikmariusz7@gmail.com>
2021-10-22 12:45:26 +0200

    initialed project weather app 2.0

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 99,
+});
+
+app.use(limiter);
+app.set("trust proxy", 1);
+
+// set static folder
+app.use(express.static("public"));
+
+// routes
+app.use("/api", require("./routes"));
+
+// enable cors
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Server is  listening at http://localhost:${PORT}`);
+});
